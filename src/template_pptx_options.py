1b90f218dba9d329a16bfba4c4685815e3ed3949
Samir Sellars <Samir.sellars@gmail.com>
2025-06-24 19:06:32 -0400

    Implement a setter and getter for the settings

--- src/template_pptx_options.py
+++ src/template_pptx_options.py
@@ -7,7 +7,17 @@ class TemplatePptxOptions:
     def __init__(self):
         self._strict = False
 
-    def set_strict_mode(self, strict_enabled: bool) -> None:
+    @property
+    def strict_mode(self) -> bool:
+        '''
+        Description: Return the value of strict mode. Strict mode
+        disables running through warnings and will cause the application to be stopped
+        and raise an error.
+        '''
+        return self._strict
+    
+    @strict_mode.setter
+    def strict_mode(self, strict_enabled: bool) -> None:
         '''
         Description: Set the strict mode of the TemplatePptx process. Strict mode
         disables running through warnings and will cause the application to be stopped
@@ -16,11 +26,3 @@ class TemplatePptxOptions:
         @input strict_enable: A boolean indicating if strict mode is enabled or not
         '''
         self._strict = strict_enabled
-
-    def strict_mode(self) -> bool:
-        '''
-        Description: Return the value of strict mode. Strict mode
-        disables running through warnings and will cause the application to be stopped
-        and raise an error.
-        '''
-        return self._strict
