c5d5309f86bf4fbf230e8f600d36d612d20c99a0
Samir Sellars <Samir.sellars@gmail.com>
2025-06-24 18:58:10 -0400

    New options to allow for further modification, also entry point for strict mode

---
+++ src/template_pptx_options.py
@@ -0,0 +1,26 @@
+
+class TemplatePptxOptions:
+    '''
+    Description: Manage the options around templatepptx
+    '''
+
+    def __init__(self):
+        self._strict = False
+
+    def set_strict_mode(self, strict_enabled: bool) -> None:
+        '''
+        Description: Set the strict mode of the TemplatePptx process. Strict mode
+        disables running through warnings and will cause the application to be stopped
+        and raise an error.
+
+        @input strict_enable: A boolean indicating if strict mode is enabled or not
+        '''
+        self._strict = strict_enabled
+
+    def strict_mode(self) -> bool:
+        '''
+        Description: Return the value of strict mode. Strict mode
+        disables running through warnings and will cause the application to be stopped
+        and raise an error.
+        '''
+        return self._strict
